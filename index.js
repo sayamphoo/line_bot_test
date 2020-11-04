e03844b020db1f30f9db8a25e61887e4cafff086
sayamphoo <Sayamphoo781@gmail.com>
2020-11-04 19:56:16 +0700

    tt

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const line = require("@line/bot-sdk");
 const express = require("express");
 const app = express();
+const port = process.env.port || 4000;
 
 app.get('/', function (x, y) {
   y.send("000")
 })
 
-const port = process.env.port || 4000;
+
 app.listen(port, () => {
   console.log("port = " + port);
 });

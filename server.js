ceaa8311fdd96b3662e4d2e5487c7586069a998f
sayamphoo <Sayamphoo781@gmail.com>
2020-11-05 13:01:15 +0700

    sa

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const line = require("@line/bot-sdk");
 const express = require("express");
-const config = require("./lineconfig/config");
 const app = express();
 
-const client = new line.Client(config.config);
+const config = {
+  channelAccessToken:
+    "lWFvEUSumTvf812IKcfbXGFlR7TeW53w0FoA94lHsi9i0drxSHWBlPiZCFuC0q1G5p27j7J2P6VUOxq0isRCIa7x/xf78UZ1aTQh4cOiUwCajV54oadRr+J9z341Y+Iy1Gfompd1qApFmVjJlAgYjAdB04t89/1O/w1cDnyilFU=",
+  channelSecret: "8d53e3fa12732a4134de5bf0e2f0a36a",
+};
+
+const client = new line.Client(config);
 
 app.post("/webhook", line.middleware(config), (req, res) => {
   Promise.all(req.body.events.map(handleEvent)).then((result) =>

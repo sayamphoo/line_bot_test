a70c41409f6482ec763ba8ff1ab935470f2a2a4e
sayamphoo <Sayamphoo781@gmail.com>
2020-11-05 21:41:43 +0700

    cccc

--- server.js
+++ server.js
@@ -7,9 +7,25 @@ const app = express();
 const client = new line.Client(co.config);
 
 app.post("/webhook", line.middleware(co.config), (req, res) => {
- 
+  Promise.all(req.body.events.map(handleEvent)).then((result) =>
+    res.json(result)
+  );
 });
 
+function handleEvent(event) {
+  if (event.type === "message") {
+    if (event.message.type === "text") {
+      replyMes(event, manager.check(event.message.text));
+    }
+  } else {
+    return Promise.resolve(null);
+  }
+}
+
+let replyMes = (e, obj) => {
+  return client.replyMessage(e.replyToken, obj);
+};
+  
 
 var server = app.listen(process.env.PORT || 3000, function () {
   var port = server.address().port;
